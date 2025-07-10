e8cca75286011e0101f9e6fae107d28f0ee0874f
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-07-10 19:23:58 +0800

    fix: Add error handling in Payment Service

--- ttbackend/app/Services/PaymentService.php
+++ ttbackend/app/Services/PaymentService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Appointment;
 use App\Models\Payment;
+use Exception;
 
 class PaymentService
 {
@@ -11,32 +12,40 @@ class PaymentService
     public function categorizePayment($appointment_id, $amount)
     {
 
-        $appointment = Appointment::query()
-            ->with('payments')
-            ->findOrFail($appointment_id);
+        try
+        {
+            $appointment = Appointment::query()
+                ->with('payments')
+                ->findOrFail($appointment_id);
+            
+            $payments = $appointment->payments
+                ->sum('amount');
+
+            $totalPayment = $payments + $amount;
+
+            $appointmentTotalPrice = $appointment->total_price;
+
+            if($totalPayment > $appointmentTotalPrice)
+            {
+                return "Overpaid";
+            }
+            elseif($totalPayment < $appointmentTotalPrice)
+            {
+                return "Pending";
+            }
+            elseif($totalPayment == $appointmentTotalPrice)
+            {
+                return "Completed";
+            }
+            else
+            {
+                return "Failed";
+            }
+        }
+        catch (Exception $e)
+        {
+            throw new Exception('Error in categorizing Payment', $e->getMessage());
+        }
         
-        $payments = $appointment->payments
-            ->sum('amount');
-
-        $totalPayment = $payments + $amount;
-
-        $appointmentTotalPrice = $appointment->total_price;
-
-        if($totalPayment > $appointmentTotalPrice)
-        {
-            return "Overpaid";
-        }
-        elseif($totalPayment < $appointmentTotalPrice)
-        {
-            return "Pending";
-        }
-        elseif($totalPayment == $appointmentTotalPrice)
-        {
-            return "Completed";
-        }
-        else
-        {
-            return "Failed";
-        }
     }
 }
